fe8417cff5822c65797058942de261f676ec8d1f
Sashumi <danielcohen448@gmail.com>
2025-06-06 15:26:30 +0200

    main and makefile added

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#define print(any) (std::cout << (any) << std::endl)
+
+int main() {
+    print(__cplusplus);
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,8 @@
+main.exe: file-main
+	g++ $(wildcard *.cpp) -o $@
+
+clean:
+	rm -f main.exe
+
+file-main:
+	touch main.cpp
