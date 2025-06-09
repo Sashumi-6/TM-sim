6b45c056d466f35797856399555ae25e6bfebd91
Sashumi <danielcohen448@gmail.com>
2025-06-09 21:57:33 +0200

    IT WORKS

--- turingMachine.h
+++ turingMachine.h
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <sstream>
 #include <fstream>
+#include <string>
 #include <iostream>
 #include "tape.h"
 
@@ -14,8 +15,78 @@ class TM {
     int tapeCount;
     tape **tapes;
     std::string *instructions;
+    int i_size = 0;
     char baseStates[3];
     char currentState;
+
+    void readInstructions(std::string filename) {
+        std::fstream fs(filename.c_str());
+        if (fs.eof()) assert("Please insert instructions");
+        std::stringstream ss;
+        std::string line;
+
+        std::getline(fs, line, '|');
+        currentState = baseStates[0] = line[0]; //initial
+        std::getline(fs, line, '|');
+        baseStates[1] = line[0]; //accept
+        std::getline(fs, line);
+        baseStates[2] = line[0]; //halt
+
+        int count = 0;
+        while (std::getline(fs, line)) {
+            if (line.empty()) continue;
+            ss << line + "\n";
+            count++;
+        }
+
+        i_size = count;
+        instructions = new std::string[count];
+        for (int i = 0 ; i < count ; i++) {
+            ss >> instructions[i];
+        }
+    }
+
+    std::string getCurrentConfiguration() {
+        std::string config(1, currentState);
+        for (int i = 0 ; i < tapeCount ; i++) {
+            config += tapes[i]->getCurrentSymbol();
+        }
+        return config;
+    }
+
+    int executeInstruction() {
+        std::string currentConfig = getCurrentConfiguration();
+
+        int instructionIdx = -1;
+        for (int i = 0 ; i < i_size ; i++) {
+            if (currentConfig != instructions[i].substr(0, tapeCount + 1)) continue;
+            instructionIdx = i;
+            break;
+        }
+        if (instructionIdx == -1) {
+            sout << "No such instruction exists for \"" << currentConfig << "\"" << send;
+            system(0);
+        }
+        sout << instructions[instructionIdx] << send;
+
+        std::string update = instructions[instructionIdx].substr(tapeCount + 2);
+        currentState = update[0];
+        int currentTape = 0;
+        bool successorSymbolSection = true;
+        for (char s : update) {
+            if (s == currentState) continue;
+
+            if (successorSymbolSection) {
+                tapes[currentTape++]->updateCell(s);
+                if (currentTape == tapeCount) {
+                    currentTape = 0;
+                    successorSymbolSection = false;
+                }
+            } else { tapes[currentTape++]->moveHead(s); } //using this bc clang doesnt like or trust me
+        }
+
+        return 1;
+    }
 
 public:
     TM(const char* input, int &tapeCount) : instructions(nullptr) {
@@ -27,7 +98,7 @@ public:
             else tapes[i] = new tape();
         }
 
-        currentState = '\0';
+        currentState = '\0'; // current state = null ie. havent set current state
     }
 
     ~TM() {
@@ -40,33 +111,12 @@ public:
     // instructions is the instructions.txt file
     int execute(std::string instructions) {
         readInstructions(instructions);
+        while (currentState != baseStates[1] && currentState != baseStates[2]) {
+            executeInstruction();
+        }
 
+        if (currentState == baseStates[1]) return 1;
         return 0;
-    }
-
-    void readInstructions(std::string filename) {
-        std::fstream fs(filename.c_str());
-        if (fs.eof()) assert("Please insert instructions");
-        std::stringstream ss;
-        std::string line;
-
-        std::getline(fs, line, '|');
-        currentState = baseStates[0] = line[0]; //initial
-        std::getline(fs, line, '|');
-        baseStates[1] = line[0]; //accept
-        std::getline(fs, line, '|');
-        baseStates[2] = line[0]; //halt
-
-        int count = 0;
-        for (line = "" ; std::getline(fs, line) ; line = "") {
-            ss << line;
-            count++;
-        }
-
-        instructions = new std::string[count];
-        for (int i = 0 ; i < count ; i++) {
-            ss >> instructions[i];
-        }
     }
 
     std::string printTapeN(int n) {
