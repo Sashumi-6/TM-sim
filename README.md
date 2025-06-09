aa288077d0c1f10683a0f3f1a95529529c4fe347
Sashumi <danielcohen448@gmail.com>
2025-06-09 21:57:55 +0200

    Final main.cpp, instructions, and readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # TM-sim
 Turing machine simulator made in C++
+
+# HOW TO USE
+
+    - The Tape:
+        The character '\0' denotes the empty cell
+
+    - The Turing Machine:
+    
+    - instructions.txt
+        FORMAT:
+            [initial state]|[accept state]|[HALT/Reject state]
+            [currect state][tape(k) currentSymbol...]:[successor state][update tape(k) symbol][Direction for tape(k)]
+            ...
+
+        example: (for a 2 tape machine)
+            q|a|h
+            q01:a10NN
+            q10:h01NN
+        For instructions, '_' denotes an empty cell
--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#define print(any) (std::cout << (any) << std::endl)
+#include "turingMachine.h"
+#define sout std::cout
+#define send std::endl
 
 int main() {
-    print(__cplusplus);
+    const char* word = "1010011";
+    int tapeCount = 2;
+    TM tm(word, tapeCount);
+    
+    sout << "\n========================================\nInitial configuration\n========================================\n" << tm.printAllTapes() << send;
+    if (tm.execute("instructions.txt")) {
+        sout << "========================================\nMachine terminates in accept state\n========================================" << send;
+    } else {
+        sout << "========================================\nMachine terminates in halt/reject state\n========================================" << send;
+    }
+    sout << tm.printAllTapes() << send;
 
     return 0;
 }
